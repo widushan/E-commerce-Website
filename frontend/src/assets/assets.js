05981dc254ff375312bfeec4c46e68658a10eca0
widushan <114930601+widushan@users.noreply.github.com>
2025-06-24 20:49:47 +0530

    route commit
    build main page routings

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -56,6 +56,7 @@ import p_img52 from './p_img52.png'
 
 
 import logo from './logo.png'
+import mylogo from './mylogo.png'
 import hero_img from './hero_img.png'
 import cart_icon from './cart_icon.png'
 import bin_icon from './bin_icon.png'
@@ -76,6 +77,7 @@ import cross_icon from './cross_icon.png'
 
 export const assets = {
     logo,
+    mylogo,
     hero_img,
     cart_icon,
     dropdown_icon,
