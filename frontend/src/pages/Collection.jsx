83e2bffdcc8385dd7f9c5c4f546fe97a1bfa5fc2
widushan <114930601+widushan@users.noreply.github.com>
2025-06-26 00:14:10 +0530

    collection commit
    Building collection part

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,7 +1,10 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets'
+import ProductItem from '../components/ProductItem';
+
+
 
 
 
@@ -9,6 +12,12 @@ const Collection = () => {
 
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
+  const [filterProducts, setFilterProducts] = useState([]);
+
+  useEffect(() => {
+    setFilterProducts(products);
+  }, []);
+
 
   return (
 
@@ -51,6 +60,35 @@ const Collection = () => {
               <input type="checkbox" className='w-3' value={'Winterwear'} />Winterwear
             </p>
           </div>
+        </div>
+
+      </div>
+
+
+      {/*Right Side Products*/}
+      <div className='flex-1'>
+
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
+
+          <Title text1={'ALL'} text2={'COLLECTIONS'} />
+
+          {/*Product Sort*/}
+          <select className='border-2 border-gray-300 text-sm px-2'>
+            <option value="relavent">Sort by: Relavent</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Lowgh</option>
+          </select>
+        </div>
+
+
+        {/*Map Products*/}
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+          {
+            filterProducts.map((item,index) => (
+              <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.image} />
+            ))
+
+          }
         </div>
 
       </div>
