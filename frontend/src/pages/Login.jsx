5a407c513fa1831974c5a6e6a78011c3e922f5a1
widushan <114930601+widushan@users.noreply.github.com>
2025-06-30 22:59:54 +0530

    login commit
    login and signup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { use, useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets'
+import { NavLink, Link } from 'react-router-dom'
+
+
 
 const Login = () => {
+
+  const [currentState, setCurrentState] = useState('Sign Up')
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  }
+
   return (
-    <div>Login</div>
+
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p  className='prata-regular text-3xl'>{currentState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
+      </div>
+
+      {currentState === 'Login' ? '' : <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required />}
+      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required />
+      <input type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required />
+
+      <div className='w-full flex justify-between text-smmt-[-8px]'>
+        <p className='cursor-pointer'>Forgot Your Password</p>
+        {
+          currentState === 'Login'
+          ? <p className='cursor-pointer' onClick={() => setCurrentState('Sign Up')}>Create Account</p>
+          : <p className='cursor-pointer' onClick={() => setCurrentState('Login')}>Already have an Account</p>
+        }
+      </div>
+
+      <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
+
+    </form>
+
   )
+
 }
 
 export default Login
