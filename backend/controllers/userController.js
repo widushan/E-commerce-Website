cc6672f2cd976d8c667e7412e040442c06d618ed
widushan <114930601+widushan@users.noreply.github.com>
2025-07-01 17:35:54 +0530

    user commit
    user registration and user login build functionally

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,79 @@
+import userModel from '../models/userModel.js';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 
+const createToken = (id) => {
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
 
 // Route for user Login
 const loginUser = async (req,res)  => {
+    try {
+        const {email,password} = req.body;
 
+        // checking user already  exists or not
+        const user = await userModel.findOne({email});
+        if (!user) {
+            return res.status(400).json({message:"User does not exists"});
+        }
+        // checking user password
+        const isMatch = await bcrypt.compare(password,user.password);
+        if (isMatch) {
+            const token = createToken(user._id)
+            res.json({sucess:true,token});
+        }
+        else{
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+
+        
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Something went wrong"});
+    }
 }
 
 
 // Route for user register
 const registerUser = async (req,res)  => {
+    try {
+        const {name,email,password} = req.body;
 
+        // checking user already  exists or not
+        const exists = await userModel.findOne({email});
+        if (exists) {
+            return res.status(400).json({message:"User already exists"});
+        }
+
+        // validating email format and strong password
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({message:"Please Enter a Valid Email"});
+        }
+        if (password.length < 8) {
+            return res.status(400).json({message:"Please Enter a Strong Password"});
+        }
+
+        // hashing user password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        // creating new user
+        const newUser = new userModel({name,email,password:hashedPassword});
+        const user = await newUser.save();
+        res.status(200).json({message:"User Created Successfully"});
+
+        const token = createToken(user._id)
+
+        res.json({sucess:true,token});
+
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Something went wrong"});
+    }
 }
 
 // Route for admin login
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 
 
 
@@ -18,6 +19,9 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoints
+app.use('/api/user',userRouter)
+
+
 app.get('/',(req,res)=>{
     res.send("API Working")
 })
