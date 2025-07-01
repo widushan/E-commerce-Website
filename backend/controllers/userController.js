9acb6f1a61e920d1c8435944bcde0bb39e8c1bc7
widushan <114930601+widushan@users.noreply.github.com>
2025-07-01 16:43:35 +0530

    routes commit
    Backend - models, Controllers, Middlewares, Routes building

---
+++ backend/controllers/userController.js
@@ -0,0 +1,22 @@
+
+
+
+// Route for user Login
+const loginUser = async (req,res)  => {
+
+}
+
+
+// Route for user register
+const registerUser = async (req,res)  => {
+
+}
+
+// Route for admin login
+const adminLogin = async (req,res) => {
+
+}
+
+
+
+export {loginUser,registerUser,adminLogin}
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+
+
+// App Config
+const app = express();
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// api endpoints
+app.get('/',(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port,()=>
+    console.log('Server Started on PORT :' + port))
+
+
