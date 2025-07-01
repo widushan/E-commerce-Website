b39431dfea7c689ca32ae98d4460f02f1f3bd9f9
widushan <114930601+widushan@users.noreply.github.com>
2025-07-01 11:10:34 +0530

    about commit
    finished about us page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { use, useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets'
+import { NavLink, Link } from 'react-router-dom'
+import Title from '../components/Title'
+import ProductItem from '../components/ProductItem'
+import NewsletterBox from '../components/NewsletterBox'
 
 const Contact = () => {
+
   return (
+
     <div>Contact</div>
+    
   )
+  
 }
 
 export default Contact
