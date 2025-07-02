4ba0c4ae9e9452f34c9dfd836989f6315403426d
widushan <114930601+widushan@users.noreply.github.com>
2025-07-02 12:19:08 +0530

    admin commit
    admin add product error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -78,7 +78,20 @@ const registerUser = async (req,res)  => {
 
 // Route for admin login
 const adminLogin = async (req,res) => {
+    try {
+        const {email,password} = req.body;
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email+password,process.env.JWT_SECRET);
+            res.json({sucess:true,token});
+        }
+        else{
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Something went wrong"});
 
+    }
 }
 
 
