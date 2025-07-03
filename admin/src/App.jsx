3baf463cd57c4b97619346faffe34c8f8c0adaa3
widushan <114930601+widushan@users.noreply.github.com>
2025-07-03 14:42:35 +0530

    8h commit
    finished 8hours from 14 hours

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,23 +1,43 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import { Routes , Route } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
+import Login from './components/Login'
+import { useState } from 'react'
 
 
 
 const App = () => {
 
+  const [token , setToken] = useState('');
+
   return (
     
     <div className='bg-gray-50 min-h-screen'>
 
+      {token === ""
+      ? <Login />
+      :
       <>
-        < Navbar />
+        <Navbar />
         <hr />
-
         <div className='flex w-full'>
           <Sidebar />
+          <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+            <Routes>
+              <Route path="/add" element={<Add />} />
+              <Route path="/list" element={<List />} />
+              <Route path='/orders' element={<Orders />} />
+            </Routes>
+          </div>
         </div>
       </>
+      }
+
+      
 
     </div>
 
