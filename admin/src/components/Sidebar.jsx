45890dcf110cd5e485e5e29fc712533a1bd9423f
widushan <114930601+widushan@users.noreply.github.com>
2025-07-03 12:04:48 +0530

    error commit
    tailwindcss error problem solved

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
 
   return (
 
-    <div>
+    <div className='w-[18%] min-h-screen border-r-2'>
         <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
             <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/add">
                 <img className='w-5 h-5' src={assets.add_icon} alt="" />
